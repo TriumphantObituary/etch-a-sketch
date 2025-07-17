0c8906dd3025b8de8fb58c76b27d186fb40bd6b8
TriumphantObituary <148290564+TriumphantObituary@users.noreply.github.com>
2025-07-17 16:43:17 +1000

    Fix grid scaling

--- scripts.js
+++ scripts.js
@@ -1,15 +1,18 @@
-initialiseGrid(16, 16);
+initialiseGrid(100);
 
-function initialiseGrid(height, width) {
+function initialiseGrid(quantity) {
+    const height = 800 / quantity;
+    const width = 800 / quantity;
     const outer = document.querySelector(".outer-container");
     /*outer.setAttribute("style", "width: calc(16 * 20px);");*/
-    for (let i = 0; i < height; i++) {
+    for (let i = 0; i < quantity; i++) {
         const container = document.createElement("div");
         container.classList.add("container");
         outer.appendChild(container);
-        for (let j = 0; j < width; j++) {
+        for (let j = 0; j < quantity; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
+            square.style.cssText = "height: " + height + "px; width: " + width + "px;";
             container.appendChild(square);
         }
     }
